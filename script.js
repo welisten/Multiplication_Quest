4ab36159b5f31e77a55fb3fe8969aec11a1fdd18
Wesley Welisten <welistenw@gmail.com>
2023-01-05 01:21:08 -0300

    Mudanças na lógica e eficiencia do arquivo js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const num1 = Math.ceil(Math.random() * 10);
-const num2 = Math.ceil(Math.random() * 10);
+// Reaproveitando o random em uma função para deixar o texto do HTML dinâmico, sem a necessidade de atualizar
+const randomNumber = function(){return Math.ceil(Math.random() * 10)}
 
 const questionEl = document.getElementById("question")
 const inputEl = document.getElementById("input")
@@ -11,23 +11,44 @@ let score = JSON.parse(localStorage.getItem("score"))
 if(!score){
     score = 0
 }
+updateContainerQuestion();
 
-scoreEl.innerText = `score: ${score}`
-questionEl.innerText = `What is ${num1} multiply by ${num2}?`
-
-const correctAns = num1 * num2;
-
-formEl.addEventListener("submit", () => {
-    const userAns = +inputEl.value
-
-    if(userAns === correctAns){
+formEl.addEventListener("submit", (event)=> {
+    event.preventDefault()
+    const userAns = Number(inputEl.value)
+    
+    if(userAns === correctAns(userAns)){
         score++
         updateLocalStorage()
+        updateContainerQuestion()
     }else{
         score--
         updateLocalStorage()
+        updateContainerQuestion()
     }
 })
+
+/* Função responsável por dinamizar os valores dentro do container 
+para que não precise atualizar a página, veja que estou reaproveitando a função randomNumber().
+Depois, passo o valor do input para vazio('') e deixo ele focado (focus()), para que o usuário já possa informar a próxima resposta.*/
+
+function updateContainerQuestion() {
+    scoreEl.innerText = `score: ${score}`
+    questionEl.innerHTML = `What is <span id="num1">${randomNumber()}</span> multiply by <span id="num2">${randomNumber()}</span>  ?`
+    inputEl.value = '';
+    inputEl.focus()
+}
+
+/* Como o texto esta dinâmico, eu só preciso pegar os valores dos 
+spans na hora que o site gerar e comparar com o valor passado pelo usuário */
+
+function correctAns(userAns){
+    const num1 = document.getElementById("num1");
+    const num2 = document.getElementById("num2");
+            // Garantindo a conversão para Number(), mesmo sabendo que o javascript já converte direto
+            // (é uma boa pratica sempre converter com funções).
+    return userAns = Number(num1.innerText * num2.innerText)
+}
 
 function updateLocalStorage(){
     localStorage.setItem("score", JSON.stringify(score))
